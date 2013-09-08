5f1c2b3565d87cf738f385656fe216df10038a88
Tomas Bosak <bosak.tomas@gmail.com>
2013-09-08 14:53:34 +0200

    Setup projects and WIP on fielt getters/setters.

---
+++ src/Docunet/Docunet.Tests/FieldTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Docunet;
+
+namespace Docunet.Tests
+{
+    [TestFixture()]
+    public class FieldTests
+    {
+        [Test()]
+        public void Should_get_and_set_bool_values()
+        {
+            var document = new Docunet()
+                .Bool("foo", true)
+                .Bool("bar.baz", false);
+
+            Assert.AreEqual(true, document.Bool("foo"));
+            Assert.AreEqual(false, document.Bool("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_byte_values()
+        {
+            var document = new Docunet()
+                .Byte("foo", 123)
+                .Byte("bar.baz", 200);
+            
+            Assert.AreEqual(123, document.Byte("foo"));
+            Assert.AreEqual(200, document.Byte("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_short_values()
+        {
+            var document = new Docunet()
+                .Short("foo", 1234)
+                .Short("bar.baz", 4321);
+            
+            Assert.AreEqual(1234, document.Short("foo"));
+            Assert.AreEqual(4321, document.Short("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_int_values()
+        {
+            var document = new Docunet()
+                .Int("foo", 123456)
+                .Int("bar.baz", 654321);
+            
+            Assert.AreEqual(123456, document.Int("foo"));
+            Assert.AreEqual(654321, document.Int("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_long_values()
+        {
+            var document = new Docunet()
+                .Long("foo", 123456789012345)
+                .Long("bar.baz", 543210987654321);
+            
+            Assert.AreEqual(123456789012345, document.Long("foo"));
+            Assert.AreEqual(543210987654321, document.Long("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_float_values()
+        {
+            var document = new Docunet()
+                .Float("foo", 123.456f)
+                .Float("bar.baz", 654.321f);
+            
+            Assert.AreEqual(123.456f, document.Float("foo"));
+            Assert.AreEqual(654.321f, document.Float("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_double_values()
+        {
+            var document = new Docunet()
+                .Double("foo", 123.456)
+                .Double("bar.baz", 654.321);
+            
+            Assert.AreEqual(123.456, document.Double("foo"));
+            Assert.AreEqual(654.321, document.Double("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_decimal_values()
+        {
+            var document = new Docunet()
+                .Decimal("foo", new Decimal(123.456))
+                .Decimal("bar.baz", new Decimal(654.321));
+            
+            Assert.AreEqual(new Decimal(123.456), document.Decimal("foo"));
+            Assert.AreEqual(new Decimal(654.321), document.Decimal("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_string_values()
+        {
+            var document = new Docunet()
+                .String("foo", "test string value")
+                .String("bar.baz", "test value string");
+            
+            Assert.AreEqual("test string value", document.String("foo"));
+            Assert.AreEqual("test value string", document.String("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_dateTime_values()
+        {
+            var dateNow = DateTime.Now;
+            var dateUtcNow = DateTime.UtcNow;
+            
+            var document = new Docunet()
+                .DateTime("foo", dateNow)
+                .DateTime("bar.baz", dateUtcNow);
+            
+            Assert.AreEqual(dateNow, document.DateTime("foo"));
+            Assert.AreEqual(dateUtcNow, document.DateTime("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_object_values()
+        {
+            var document = new Docunet()
+                .Object("foo", null)
+                .Object("bar.baz", 123);
+            
+            Assert.AreEqual(null, document.Object("foo"));
+            Assert.AreEqual(123, document.Object("bar.baz"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_document()
+        {
+            var doc = new Docunet()
+                .String("foo", "test string value")
+                .String("bar.baz", "test value string");
+            
+            var document = new Docunet()
+                .Document("doc", doc);
+            
+            Assert.AreEqual(doc, document.Document("doc"));
+        }
+        
+        [Test()]
+        public void Should_get_and_set_flat_list()
+        {
+            var stringList = new List<string> { "one", "two", "three" };
+            var intList = new List<int> { 1, 2, 3 };
+            
+            var document = new Docunet()
+                .List("foo", stringList)
+                .List("bar.baz", intList);
+            
+            Assert.AreEqual(stringList, document.List<string>("foo"));
+            Assert.AreEqual(intList, document.List<int>("bar.baz"));
+        }
+    }
+}
---
+++ src/Docunet/Docunet/Docunet.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Docunet
+{
+    public class Docunet : Dictionary<string, object>
+    {
+        #region Field getters
+        
+        public bool Bool(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (bool)value;
+            }
+        }
+        
+        public byte Byte(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (byte)value;
+            }
+        }
+        
+        public short Short(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (short)value;
+            }
+        }
+        
+        public int Int(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (int)value;
+            }
+        }
+        
+        public long Long(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (long)value;
+            }
+        }
+        
+        public float Float(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (float)value;
+            }
+        }
+        
+        public double Double(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (double)value;
+            }
+        }
+        
+        public decimal Decimal(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (decimal)value;
+            }
+        }
+        
+        public string String(string fieldPath)
+        {
+            return (string)GetField(fieldPath);
+        }
+        
+        public DateTime DateTime(string fieldPath)
+        {
+            return (DateTime)GetField(fieldPath);
+        }
+        
+        public object Object(string fieldPath)
+        {
+            return (object)GetField(fieldPath);
+        }
+        
+        public Docunet Document(string fieldPath)
+        {
+            return (Docunet)GetField(fieldPath);
+        }
+        
+        public List<T> List<T>(string fieldPath)
+        {
+            var value = GetField(fieldPath);
+            
+            if (value == null)
+            {
+                throw new Exception("Value is null.");
+            }
+            else
+            {
+                return (List<T>)value;
+            }
+        }
+        
+        private object GetField(string fieldPath)
+        {
+            if (fieldPath.Contains("."))
+            {
+                var fields = fieldPath.Split('.');
+                var iteration = 1;
+                var embeddedDocument = this;
+
+                foreach (var field in fields)
+                {
+                    if (iteration == fields.Length)
+                    {
+                        if (embeddedDocument.ContainsKey(field))
+                        {
+                            return embeddedDocument[field];
+                        }
+                        
+                        break;
+                    }
+
+                    if (embeddedDocument.ContainsKey(field))
+                    {
+                        embeddedDocument = (Docunet)embeddedDocument[field];
+                    }
+                    else
+                    {
+                        // if current field in path isn't present
+                        break;
+                    }
+
+                    iteration++;
+                }
+            }
+            else
+            {
+                if (this.ContainsKey(fieldPath))
+                {
+                    return this[fieldPath];
+                }
+            }
+            
+            return null;
+        }
+        
+        #endregion
+        
+        #region Field setters
+        
+        public Docunet Bool(string fieldPath, bool value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Byte(string fieldPath, byte value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Short(string fieldPath, short value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Int(string fieldPath, int value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Long(string fieldPath, long value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Float(string fieldPath, float value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Double(string fieldPath, double value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Decimal(string fieldPath, decimal value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet String(string fieldPath, string value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet DateTime(string fieldPath, DateTime value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Object(string fieldPath, object value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet Document(string fieldPath, Docunet value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        public Docunet List<T>(string fieldPath, List<T> value)
+        {
+            SetField(fieldPath, value);
+
+            return this;
+        }
+        
+        private void SetField(string fieldPath, object value)
+        {
+            if (fieldPath.Contains("."))
+            {
+                var fields = fieldPath.Split('.');
+                var iteration = 1;
+                var embeddedDocument = this;
+
+                foreach (var field in fields)
+                {
+                    if (iteration == fields.Length)
+                    {
+                        if (embeddedDocument.ContainsKey(field))
+                        {
+                            embeddedDocument[field] = value;
+                        }
+                        else
+                        {
+                            embeddedDocument.Add(field, value);
+                        }
+                        
+                        break;
+                    }
+
+                    if (embeddedDocument.ContainsKey(field))
+                    {
+                        embeddedDocument = (Docunet)embeddedDocument[field];
+                    }
+                    else
+                    {
+                        // if document which contains the field doesn't exist create it first
+                        var tempDocument = new Docunet();
+                        embeddedDocument.Add(field, tempDocument);
+                        embeddedDocument = tempDocument;
+                    }
+
+                    iteration++;
+                }
+            }
+            else
+            {
+                if (this.ContainsKey(fieldPath))
+                {
+                    this[fieldPath] = value;
+                }
+                else
+                {
+                    this.Add(fieldPath, value);
+                }
+            }
+        }
+        
+        #endregion
+    }
+}

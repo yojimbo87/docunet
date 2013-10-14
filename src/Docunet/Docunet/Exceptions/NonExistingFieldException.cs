da61a24d6e0fe239e9731ffe58845a283f624411
Tomas Bosak <bosak.tomas@gmail.com>
2013-10-14 18:52:46 +0200

    Add exceptions which represent null and non existing field errors.

---
+++ src/Docunet/Docunet/Exceptions/NonExistingFieldException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Docunet
+{
+    public class NonExistingFieldException : Exception
+    {
+        public NonExistingFieldException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/Docunet/Docunet/Exceptions/NullFieldException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Docunet
+{
+    public class NullFieldException : Exception
+    {
+        public NullFieldException(string message) : base(message)
+        {
+        }
+    }
+}

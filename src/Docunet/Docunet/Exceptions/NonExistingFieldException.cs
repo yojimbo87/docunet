ed8944aa9fb85cb36d5c91561b3374990eb599e1
Tomas Bosak <bosak.tomas@gmail.com>
2013-10-22 21:05:54 +0200

    Add xml docs to exception classes.

--- src/Docunet/Docunet/Exceptions/NonExistingFieldException.cs
+++ src/Docunet/Docunet/Exceptions/NonExistingFieldException.cs
@@ -2,6 +2,9 @@
 
 namespace Docunet
 {
+    /// <summary> 
+    /// Represents error which happens when document field does not exist.
+    /// </summary>
     public class NonExistingFieldException : Exception
     {
         public NonExistingFieldException(string message) : base(message)
--- src/Docunet/Docunet/Exceptions/NullFieldException.cs
+++ src/Docunet/Docunet/Exceptions/NullFieldException.cs
@@ -2,6 +2,9 @@
 
 namespace Docunet
 {
+    /// <summary> 
+    /// Represents error which happens when document field has null value.
+    /// </summary>
     public class NullFieldException : Exception
     {
         public NullFieldException(string message) : base(message)
